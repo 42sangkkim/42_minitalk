d7c4310822d0e0603bf89b558add70aea6e2dd07
sangkkim <madlogic365@gmail.com>
2022-07-11 11:52:54 +0900

    change comment

--- bonus/utils_bonus.h
+++ bonus/utils_bonus.h
@@ -6,7 +6,7 @@
 /*   By: sangkkim <sangkkim@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/11 01:32:11 by sangkkim          #+#    #+#             */
-/*   Updated: 2022/07/11 11:46:44 by sangkkim         ###   ########.fr       */
+/*   Updated: 2022/07/11 11:52:35 by sangkkim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,14 @@
 # include <unistd.h>
 # include <signal.h>
 
-// utils1.c
+// utils1_bonus.c
 void	print(const char *s);
 void	print_error(char *msg);
 void	print_warning(char *msg);
 void	print_success(char *msg);
 void	put_unbr(unsigned int n);
 
-// utils2.c
+// utils2_bonus.c
 int		init_sigaction(struct sigaction *sa, \
 		void (*handler)(int, siginfo_t *, void *));
 
